eea82c73f537e6653ea4c7128066cebe2c485a38
unknown <mikrajewski@697-334-Edoras.sf.k12.internal>
2015-09-01 11:37:11 -0700

    Lightning v1

--- Lightning.pde
+++ Lightning.pde
@@ -1,20 +1,30 @@
-int x = 150;
-int y = 5;
+int startX = 150;
+int startY = 0;
+int endX = 150;
+int endY = 0;
 void setup()
 {
   size(300,300);
   background(0);
-  noLoop();
+  strokeWeight(5);
 }
 void draw()
 {
-	stroke(255,0,0);
-	line(150,0,x,y);
-	if(mousePressed == true)
+	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+	while(endY < 300)
 	{
-		x = x + (int)(Math.random()*20)-10;
-		y = y + (int)(Math.random()*20)-10;
+		endX = startX + (int)(Math.random()*20)-10;
+		endY = startY + (int)(Math.random()*10);
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
 	}
 
 }
-
+void mousePressed()
+{
+	startX = 150;
+	startY = 0;
+	endX = 150;
+	endY = 0;
+}
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,26 +14,36 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int x = 150;
-int y = 5;
+int startX = 150;
+int startY = 0;
+int endX = 150;
+int endY = 0;
 public void setup()
 {
   size(300,300);
   background(0);
-  noLoop();
+  strokeWeight(5);
 }
 public void draw()
 {
-	stroke(255,0,0);
-	line(150,0,x,y);
-	if(mousePressed == true)
+	stroke((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
+	while(endY < 300)
 	{
-		x = x + (int)(Math.random()*20)-10;
-		y = y + (int)(Math.random()*20)-10;
+		endX = startX + (int)(Math.random()*20)-10;
+		endY = startY + (int)(Math.random()*10);
+		line(startX,startY,endX,endY);
+		startX = endX;
+		startY = endY;
 	}
 
 }
-
+public void mousePressed()
+{
+	startX = 150;
+	startY = 0;
+	endX = 150;
+	endY = 0;
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
